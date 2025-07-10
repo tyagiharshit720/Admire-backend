1e42bec1f3292e0a072a53609ac1da79f0586e1c
Dhiraj Shah <shahdhiraj0002@gmail.com>
2025-07-10 12:02:33 +0530

    update the validation

--- src/controller/destination.controller.js
+++ src/controller/destination.controller.js
@@ -1,4 +1,9 @@
+import usePrams from 'use-params';
 
 export const destination=async(req, res) => {
-    
+   const { type } = usePrams(req);
+}
+export const itinerary=async(req,res)=>{
+    const { place } = usePrams(req);
+     
 }
---
+++ src/routes/destination.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { destination, itinerary } from '../controller/destination.controller.js';
+
+const routes=express.Router();
+
+routes.get('/:type',destination);
+routes.get('/trending-destinatiojn/:place',itinerary);
