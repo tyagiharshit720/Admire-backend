c04bead8ca45b4aa255824685beb1bd43240a3b7
Dhiraj Shah <shahdhiraj0002@gmail.com>
2025-07-11 17:43:22 +0530

    minor chnage

--- src/validate/leads.validate.js
+++ src/validate/leads.validate.js
@@ -4,8 +4,6 @@ export const LeadsValidator = [
   body('email').isEmail().withMessage('Valid email is required'),
   body('phone').notEmpty().withMessage('Mobile Number is required'),
   body('phone').isMobilePhone('en-IN').withMessage('Mobile Number is invalid'),
-  // body('message').isLength({min:10}).withMessage('Message must be at least 10 characters long'),
-  // body('subject').notEmpty().withMessage('Subject is required'),
-  // body('subject').isLength({min:3}).withMessage('Subject must be at least 3 characters long'),
+  
   body('destination').notEmpty().withMessage('Destination is required'),
 ];
