dd561a0ae4cf5e732cf45480785fe3ba885592bf
LearnerSumit <sumit.202104177@tulas.edu.in>
2025-07-11 18:09:43 +0530

    correction in validator fuction

--- src/routes/leads.route.js
+++ src/routes/leads.route.js
@@ -8,13 +8,18 @@ import {
 } from '../controller/leads.controller.js';
 
 import { validate } from '../middleware/validator.js';
-import { LeadsValidator } from '../validate/leads.validate.js';
+import {
+  contactValidator,
+  planYourJourneyValidator,
+  subscribeValidator,
+  suggestionComplainValidator,
+} from '../validate/leads.validate.js';
 
 const router = express.Router();
 
-router.post('/planYourJourney', LeadsValidator, validate, planYourJourney);
-router.post('/contact', LeadsValidator, validate, contact);
-router.post('/subscribe', LeadsValidator, validate, subscribe);
-router.post('/suggestionComplain', LeadsValidator, validate, suggestionComplain);
+router.post('/planYourJourney', planYourJourneyValidator, validate, planYourJourney);
+router.post('/contact', contactValidator, validate, contact);
+router.post('/subscribe', subscribeValidator, validate, subscribe);
+router.post('/suggestionComplain', suggestionComplainValidator, validate, suggestionComplain);
 
 export default router;
--- src/validate/leads.validate.js
+++ src/validate/leads.validate.js
@@ -1,11 +1,28 @@
 import { body } from 'express-validator';
-export const LeadsValidator = [
-  body('name').notEmpty().withMessage('Name is required'),
-  body('email').isEmail().withMessage('Valid email is required'),
-  body('phone').notEmpty().withMessage('Mobile Number is required'),
+
+export const planYourJourneyValidator = [
+  body('name').trim().notEmpty().withMessage('Name is required'),
+  body('email').trim().isEmail().withMessage('Valid email is required'),
+  body('phone').trim().notEmpty().withMessage('Mobile Number is required'),
   body('phone').isMobilePhone('en-IN').withMessage('Mobile Number is invalid'),
-  // body('message').isLength({min:10}).withMessage('Message must be at least 10 characters long'),
-  // body('subject').notEmpty().withMessage('Subject is required'),
-  // body('subject').isLength({min:3}).withMessage('Subject must be at least 3 characters long'),
-  body('destination').notEmpty().withMessage('Destination is required'),
+  body('destination').trim().notEmpty().withMessage('Destination is required'),
+];
+
+export const contactValidator = [
+  body('name').trim().notEmpty().withMessage('Name is required'),
+  body('email').trim().isEmail().withMessage('Valid email is required'),
+  body('message').trim().notEmpty().withMessage('Message is required'),
+  body('subject').trim().notEmpty().withMessage('Subject is required'),
+];
+
+export const subscribeValidator = [
+  body('name').trim().notEmpty().withMessage('Name is required'),
+  body('phone').trim().notEmpty().withMessage('Mobile Number is required'),
+  body('email').trim().isEmail().withMessage('Valid email is required'),
+];
+
+export const suggestionComplainValidator = [
+  body('name').trim().notEmpty().withMessage('Name is required'),
+  body('email').trim().isEmail().withMessage('Valid email is required'),
+  body('message').trim().notEmpty().withMessage('Message is required'),
 ];
