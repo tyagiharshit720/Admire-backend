14436b87152f2a33ee6539ae1541c00baa4235c1
admiregroup2025 <info@admiregroup.org>
2025-07-12 11:31:44 +0530

    Update ValidateResult.js

--- src/middleware/ValidateResult.js
+++ src/middleware/ValidateResult.js
@@ -1,9 +1,1 @@
-import { validationResult } from 'express-validator';
 
-export const validateResult = (req) => {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return result.array();
-  }
-  return null;
-};
